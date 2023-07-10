6f37b08afc715fc2ac1df89af59da975aa152c7e
spicke23 <94138528+spicke23@users.noreply.github.com>
2023-07-10 01:33:35 -0400

    Update StudentServiceImplTest.java
    Agregamos imports.

--- src/test/java/com/kibernumacademy/devops/services/StudentServiceImplTest.java
+++ src/test/java/com/kibernumacademy/devops/services/StudentServiceImplTest.java
@@ -14,6 +14,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class StudentServiceImplTest {
 
